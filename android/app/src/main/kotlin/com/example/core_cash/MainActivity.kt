fb8831f9e1f2a61f7c8b2125f22fffd6e0144079
MarinoBotina258 <marinorubianobotina@gmail.com>
2024-04-11 01:15:33 -0500

    Scafolding creado

---
+++ android/app/src/main/kotlin/com/example/core_cash/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.core_cash
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/utils/constants/image_strings.dart
@@ -0,0 +1,7 @@
+class AppImage {
+  AppImage._();
+  // Onboarding images
+  static const String loginImage1 =
+      "assets/images/login_images/group291.svg";
+
+}
